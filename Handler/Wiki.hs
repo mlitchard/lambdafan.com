240024190855a2ab1172e89e19a538f41122f221
Michael Litchard <michael@schmong.org>
2015-12-04 07:08:30 -0800

    projects points to github for now

--- Handler/Wiki.hs
+++ Handler/Wiki.hs
@@ -1,6 +1,5 @@
 module Handler.Wiki
     ( getWikiHomeR
-    , getWikiR
     ) where
 
 import Import
@@ -8,11 +7,4 @@ import Network.HTTP.Types (status301)
 import qualified Data.Text as T
 
 getWikiHomeR :: Handler Html
-getWikiHomeR = redirect ("https://github.com/yesodweb/yesod-cookbook" :: T.Text)
-
-getWikiR :: Text -> Handler Html
-getWikiR path = redirect $ T.concat
-    [ "https://github.com/yesodweb/yesod-cookbook/blob/master/cookbook/"
-    , path
-    , ".md"
-    ]
+getWikiHomeR = redirect ("https://github.com/mlitchard" :: T.Text)
